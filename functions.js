735524dd8558f66d2d9d8be660ad6a50ac19188d
Isaac Wilson <iswilson777@gmail.com>
2023-03-17 22:07:50 +0000

    added functions

---
+++ functions.js
@@ -0,0 +1,14 @@
+//function with two parameters and return statement
+function add(a, b)
+{
+  return a + b;
+}
+
+//function with one parameter no return statement
+function print(func) 
+{
+  console.log(func);
+}
+
+//function call
+print(add(5,7));
