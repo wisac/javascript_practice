82cae0e86714f004d4c3648ebd94c42dfb522087
Isaac Wilson <iswilson777@gmail.com>
2023-06-08 14:58:18 +0000

    update: defining object properties using symbols

--- 9-objects.js
+++ 9-objects.js
@@ -780,4 +780,26 @@ console.log(Animal._name); // sheep Getter called in an asssignment statement
 
 
 
-         
+    /* USING SYMBOLS TO DEFINE OBJECT PROPERTIES */
+
+    /* Symbols can be used define unique names for object properties. These properties in turn are only read using these symbols with the bracket notation. 
+    */
+
+        const firstName = Symbol('firstName');
+        const lastName = Symbol('lastName');
+        const person = {};
+        person[firstName] = 'John';
+        person[lastName] = 'Doe';
+        console.log(person[firstName]);// "John"
+        console.log(person[lastName]); // "Doe"
+        console.log(person[0]); // undefined
+        console.log(person[1]); // undefined
+        console.log(person.firstName); // undefined
+        console.log(person.lastName); // undefined
+        console.log(person['firstName']); // undefined
+        console.log(person['lastName']);// undefined  
+
+
+
+
+        
