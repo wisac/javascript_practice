2536e88c8397ad57f8bf615201810536882fb9a0
Isaac Wilson <iswilson777@gmail.com>
2023-06-15 00:14:26 +0000

    update: working with arrays; sorting, access

--- 10-working_with_arrays.js
+++ 10-working_with_arrays.js
@@ -1,6 +1,6 @@
 /*1. CREATING ARRAYS
     1. Array literal notation
-    2. Via constructor function  
+    2. Via constructor function
 
  literal notation
 
@@ -32,7 +32,7 @@ let arr3 = new Array("mango"); //creates an array with one element
         unshift() : Adds element at the beginning and return new length
 
         splice() : Inserts elements at specific index, can delete before inserting new elements. It returns an array of the removed elements
-            
+
             syntax:
                 arr.splice(start index, number of elem to be deleted, elements to insert);
 
@@ -41,7 +41,7 @@ let arr3 = new Array("mango"); //creates an array with one element
 */
 
     arr2.push(8,9); // 4
-    console.log(arr2.pop()); 
+    console.log(arr2.pop());
     arr1.shift();
     arr1.unshift(9,0,6,2,4); // 9, 0, ...
     arr1.push(99);
@@ -50,15 +50,15 @@ let arr3 = new Array("mango"); //creates an array with one element
 
     arr1.splice(2,0,77,88); // inserts 77 and 88 from [2]. Other elements are shifted backwards
     console.log(arr1);
-    
+
     arr1.splice(3,1,-4); // removes 1 element from index 3 and insert -4 at index 3
 
-    console.log(arr1);  
+    console.log(arr1);
 
 
     /* COPY ELEMENTS FROM AN ARRAY */
-    /*The slice() method is used to copy elements from an array. It returns an array containing the copied elements. 
-    
+    /*The slice() method is used to copy elements from an array. It returns an array containing the copied elements.
+
     It takes two arguments :
     @param0 - the index from which the extraction should start.
     @param1 = the index from which the extraction should end. If this is not supplied, extraction ends at end of array.
@@ -69,7 +69,7 @@ let arr3 = new Array("mango"); //creates an array with one element
     console.log(`Slided = ${names.slice(1,4)}`); // Luke, lily, Ethan
     console.log("Names = ", names);
 
-    console.log(names.slice(2,-1)); // from 2 to 4 
+    console.log(names.slice(2,-1)); // from 2 to 4
     console.log(names.slice(0,-3));
     console.log(names.slice(-4, -1)); 1, 4 // lily ethan
     console.log(names.slice(-2,1)); // ; // 3, 1 Not work
@@ -79,7 +79,7 @@ let arr3 = new Array("mango"); //creates an array with one element
             If only one args is given, if positive, start from that index and extract till the end inclusive
             If negative, start from the back and extract last inclusive
 
-            If two args are given, if negative add length to start. 
+            If two args are given, if negative add length to start.
 
             @end
                 If negative, add length ot it and exclude end index
@@ -90,21 +90,133 @@ let arr3 = new Array("mango"); //creates an array with one element
             */
 
 
+    /*SORTING ARRAYS
+      The sort and reverse methods are available for use in sorting an array.
+      The reverse method reverses the array whilst the sort method sorts the array in a specific order.
+    */
+
+     fruits = ["orange", "mango", "melon", "pear", "berries"];
+     fruits.reverse();
+     console.log(fruits); //berries, pear, melon, mango, orange
+
+
+     /*Using sort method */
+    /* We can use the sort method with comparison functions in order to specify the cateria for sorting elements in an array.
+    The comparison function is then passed as an argument to the array.sort method
+    */
+
+    ages = [74,45,7,9,25,6,13,12];
+
+    function compare(value1, value2)
+    {
+        if (value1 > value2)
+        {
+            return 1;
+        }
+        else if (value1 < value2)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    ages.sort(compare); //sorting ages in ascending order
+    console.log(ages); // output: 6, 7, 9, 12, 13, 25, 45, 74
+
+
+    /*NB: In Javascript, we can pass functions as arguments to other functions. When doing so, the () is omited so that that return value instead would not be passed.
+    */
+
+
+
+/* USING ARRAYS AS STACK */
+/* Stack can be implemeted in JS by using arrays. To do this, we use the push and pop method of the array object.
+
+
+Application of stack
+     Undo and redo action of a user
+*/
+
+const stack = []; /// Declare an empty array
+
+stack.push(4);
+stack.push(5,7,4,3,7);
+console.log(stack);
+stack.pop();
+console.log(stack);
+
+
+/* USING ARRAYS AS QUEUES
+    Queue is a linear data structure and works on the FIFO principle.
+    Queue can be implemented in js by using arrays and push and shift methods of array.
+
+    Application of queues
+        Message queues
+    
+*/
+
+const queue = [];
+queue.push(12);
+queue.push(5,32,6,9);
+console.log(queue);
+queue.shift();
+console.log(queue);
+
+
+/*FINDING ELEMENTS IN AN ARRAY 
+
+indexOf() - returns the index of its argument, a second argument can be used to specify where the search should begin. It returns -1 if element was not found
+
+lastIndexOf() - It searches from the end of the array and return the index of the argument if present. Otherwise, returns -1;
+*/
+
+console.log(queue.indexOf(6));
+console.log(queue.lastIndexOf(6));
+
+
+    /*Using arrays.includes() method
+        As of ES2016, the includes() method is used to check whether an arrat contains the passed values or not. It returns a boolean.
+        
+        Addtional @para can be used to specify where the search should begiin.
+
+    */
+
+    console.log(queue.includes(13)); // false
+
 
     
-    
 
 
-    
-
-    
+ 
 
 
 
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
