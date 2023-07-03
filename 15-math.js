b716335b7f651df558d3c7a7c15ba60c4de671f4
Isaac Wilson <iswilson.dev@gmail.com>
2023-07-03 16:51:47 +0000

    feat: The Math object

---
+++ 15-math.js
@@ -0,0 +1,18 @@
+/* USING THE MATH OBJECT 
+ The math object provides many mathematical methods for carrying out complex math calculations
+
+ Math.ceil() - ceil a number
+ Math.floor() - floor a number
+ Math.round() - Rounds a number to the nearest whole integer
+ Math.max() - Returns max of its arguments
+ Math.min() - ''       min   ''     ''      
+ Math.random() - Returns a random number between 0 and 1
+ Math.sqrt() - 
+ */
+
+ console.log(Math.round(4.53))
+ console.log(Math.round(Math.random() * 10))
+ console.log(Math.sqrt(100))
+ console.log(Math.min(3,5,7,2,7));
+ console.log(Math.pow(4,2)); // same as x ** y
+ 
