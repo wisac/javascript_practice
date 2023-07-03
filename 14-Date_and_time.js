80609da476c7b6b416b308df54d8a735437c25e0
Isaac Wilson <iswilson.dev@gmail.com>
2023-07-03 16:41:51 +0000

    feat: using date object

---
+++ 14-Date_and_time.js
@@ -0,0 +1,20 @@
+/* WORKING WITH DATE AND TIME INFORMATION
+The Date object is avalable to create an instance of date object from it by calling the constructor function.
+
+You can pass infromation to the constructor function. Without options, the date created is of the date and time of the device
+The Date object has many methods used for getting specific date or time information
+
+*/
+
+let today = new Date();
+console.log(today);
+
+console.log(today.getDate())
+console.log(today.getDay())
+console.log(today.getFullYear());
+console.log(today.getTimezoneOffset(-4));
+
+
+
+
+
